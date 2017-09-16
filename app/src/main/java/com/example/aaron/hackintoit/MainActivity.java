fb7bb8f1d3af7d0ac94d9c7e6df0ecd53973519c
Yena Kim <yenakim@berkeley.edu>
2017-09-16 02:41:30 -0700

    got the closest location

--- app/src/main/java/com/example/aaron/hackintoit/MainActivity.java
+++ app/src/main/java/com/example/aaron/hackintoit/MainActivity.java
@@ -1,12 +1,37 @@
 package com.example.aaron.hackintoit;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = CurrLocationActivity.class.getSimpleName();
+    private PlaceDetectionClient mPlaceDetectionClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+
+    // Used for selecting the current place.
+    private String mLikelyPlaceNames;
+
+    private TextView loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +49,61 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(myToolbar);
 
+    }
+
+    public void getLoc(View view) {
+        System.out.println("ASD");
+
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        if (!mLocationPermissionGranted) {
+            getLocationPermission();
+        }
+
+        if (mLocationPermissionGranted) {
+            // Get the likely places - that is, the businesses and other points of interest that
+            // are the best match for the device's current location.
+            @SuppressWarnings("MissingPermission") final
+            Task<PlaceLikelihoodBufferResponse> placeResult =
+                    mPlaceDetectionClient.getCurrentPlace(null);
+            placeResult.addOnCompleteListener
+                    (new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                        @Override
+                        public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                System.out.println("HJSDFHLKJSDF");
+
+                                PlaceLikelihood place = task.getResult().get(0);
+
+                                mLikelyPlaceNames = (String) place.getPlace().getName();
+
+                                loc = (TextView)findViewById(R.id.money_left);
+                                loc.setText(mLikelyPlaceNames);
+
+                            } else {
+                                Log.e(TAG, "Exception: %s", task.getException());
+                            }
+                        }
+                    });
+        }
+
+    }
+
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 }
